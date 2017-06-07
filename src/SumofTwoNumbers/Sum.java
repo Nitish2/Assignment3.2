487c6fa8c24fa30857ab36f445f4d3e9ca033f9d
Nitish2 <nitishn886@gmail.com>
2017-06-07 14:42:21 +0530

    Assignment3.2

---
+++ src/SumofTwoNumbers/Sum.java
@@ -0,0 +1,14 @@
+package SumofTwoNumbers;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 67; 
+		int b = 33;
+		int sum = b-(-a);
+		System.out.println("Sum of two numbers is: " +sum);
+		
+	}
+
+}
